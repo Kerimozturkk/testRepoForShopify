76c43153797aeb199de77ba1cdead6a1131f5cc6
Kerim Ozturk <49280604+Kerimozturkk@users.noreply.github.com>
2022-05-13 12:27:41 +0300

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,16 +1,5 @@
-jQuery(document).ready(function() {	
-		$('body').prepend('<div class="header" id="myHeader"><h1> teste </h1></div>');
-		$('head').prepend('<style>.header { padding: 10px 16px; background: #555; color: #f1f1f1; } .content { padding: 16px; } .sticky { position: fixed; top: 0; width: 100%} .sticky + .content { padding-top: 102px; }</style>');
+const para = document.createElement("h2");
+const node = document.createTextNode("Başlık Eklendi");
 
-		var header = document.getElementById("myHeader");
-		var sticky = header.offsetTop;
-
-		window.onscroll = function() { 
-			if (window.pageYOffset > sticky) {
-				header.classList.add("sticky");
-			} else {
-				header.classList.remove("sticky");
-			}
-		};
-  
-});
+para.appendChild(node);
+document.getElementsByClassName("gradient").appendChild(para);
