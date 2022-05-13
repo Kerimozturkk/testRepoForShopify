269c81af39b5c03dbf1aae18cbb5ece8dd78e5a4
Kerim Ozturk <49280604+Kerimozturkk@users.noreply.github.com>
2022-05-13 12:17:49 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-document.body.innerHTML = '<div style="position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;">Merhaba Dostlar</div>';
+jQuery(document).ready(function() {	
+		$('body').prepend('<div class="header" id="myHeader"><h1> teste </h1></div>');
+		$('head').prepend('<style>.header { padding: 10px 16px; background: #555; color: #f1f1f1; } .content { padding: 16px; } .sticky { position: fixed; top: 0; width: 100%} .sticky + .content { padding-top: 102px; }</style>');
+
+		var header = document.getElementById("myHeader");
+		var sticky = header.offsetTop;
+
+		window.onscroll = function() { 
+			if (window.pageYOffset > sticky) {
+				header.classList.add("sticky");
+			} else {
+				header.classList.remove("sticky");
+			}
+		};
+  
+});
